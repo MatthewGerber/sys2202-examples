5861f7d16a7bc2564d6b5e9b3b3ab075d0edf159
MatthewGerber <gerber.matthew@gmail.com>
2017-04-06 10:07:29 -0400

    Updated S3 example.

--- src/sys2202/aws/s3/Sample.java
+++ src/sys2202/aws/s3/Sample.java
@@ -27,10 +27,6 @@ import java.util.UUID;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
@@ -62,22 +58,7 @@ public class Sample {
 
 	public static void main(String[] args) throws IOException {
 
-		/*
-		 * The ProfileCredentialsProvider will return your [default] credential
-		 * profile by reading from the credentials file located at
-		 * (~/.aws/credentials).
-		 */
-		AWSCredentials credentials = null;
-		try {
-			credentials = new ProfileCredentialsProvider().getCredentials();
-		} catch (Exception e) {
-			throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
-					+ "Please make sure that your credentials file is at the correct "
-					+ "location (~/.aws/credentials), and is in valid format.", e);
-		}
-
 		AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-		s3.setRegion(Region.getRegion(Regions.US_EAST_1));
 
 		String bucketName = "my-first-s3-bucket-" + UUID.randomUUID();
 		String key = "MyObjectKey";
@@ -87,6 +68,7 @@ public class Sample {
 		System.out.println("===========================================\n");
 
 		try {
+			
 			/*
 			 * Create a new S3 bucket - Amazon S3 bucket names are globally
 			 * unique, so once a bucket name has been taken by any user, you
@@ -144,8 +126,7 @@ public class Sample {
 			 * operation to retrieve additional results.
 			 */
 			System.out.println("Listing objects");
-			ObjectListing objectListing = s3
-					.listObjects(new ListObjectsRequest().withBucketName(bucketName).withPrefix("My"));
+			ObjectListing objectListing = s3.listObjects(new ListObjectsRequest().withBucketName(bucketName).withPrefix("My"));
 			for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
 				System.out.println(" - " + objectSummary.getKey() + "  " + "(size = " + objectSummary.getSize() + ")");
 			}
@@ -166,18 +147,19 @@ public class Sample {
 			 */
 			System.out.println("Deleting bucket " + bucketName + "\n");
 			s3.deleteBucket(bucketName);
+			
 		} catch (AmazonServiceException ase) {
-			System.out.println("Caught an AmazonServiceException, which means your request made it "
-					+ "to Amazon S3, but was rejected with an error response for some reason.");
+			
+			System.out.println("Caught an AmazonServiceException, which means your request made it to Amazon S3, but was rejected with an error response for some reason.");
 			System.out.println("Error Message:    " + ase.getMessage());
 			System.out.println("HTTP Status Code: " + ase.getStatusCode());
 			System.out.println("AWS Error Code:   " + ase.getErrorCode());
 			System.out.println("Error Type:       " + ase.getErrorType());
 			System.out.println("Request ID:       " + ase.getRequestId());
+			
 		} catch (AmazonClientException ace) {
-			System.out.println("Caught an AmazonClientException, which means the client encountered "
-					+ "a serious internal problem while trying to communicate with S3, "
-					+ "such as not being able to access the network.");
+			
+			System.out.println("Caught an AmazonClientException, which means the client encountered a serious internal problem while trying to communicate with S3, such as not being able to access the network.");
 			System.out.println("Error Message: " + ace.getMessage());
 		}
 	}
