e575bab3764eabaa7b9b16ff384e283e1ba6422b
MatthewGerber <gerber.matthew@gmail.com>
2017-04-24 22:09:18 -0400

    Whitespace.

--- src/sys2202/hadoop/mapreduce/csv/NameCountMapper.java
+++ src/sys2202/hadoop/mapreduce/csv/NameCountMapper.java
@@ -15,34 +15,33 @@ import com.opencsv.CSVParser;
 public class NameCountMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
 	private CSVParser parser;
-	
+
 	public NameCountMapper() {
-		
+
 		parser = new CSVParser();
-		
+
 	}
-	
+
 	@Override
 	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-		
-		// the CSV file has a header row, which needs to be skipped. the keys for text input files
-		// are the file positions, so the header row corresponds to a key of zero. skip this line.
-		if(key.get() == 0)
-		{
+
+		// the CSV file has a header row, which needs to be skipped. the keys for text input 
+		// files are the file positions, so the header row corresponds to a key of zero. skip this line.
+		if (key.get() == 0) {
 			return;
 		}
-		
+
 		// parse the record
 		String csvLine = value.toString();
-        String[] record = parser.parseLine(csvLine);
-        
-        // create the intermediate key
-        Text firstName = new Text(record[1]);
-        
-        // create the intermediate value
-        IntWritable one = new IntWritable(1);
-        
-        // emit the intermediate key-value pair
-        output.collect(firstName, one);
+		String[] record = parser.parseLine(csvLine);
+
+		// create the intermediate key
+		Text firstName = new Text(record[1]);
+
+		// create the intermediate value
+		IntWritable one = new IntWritable(1);
+
+		// emit the intermediate key-value pair
+		output.collect(firstName, one);
 	}
 }
