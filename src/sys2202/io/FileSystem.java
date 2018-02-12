2be8c7e2d69932ad0c659a6605682e7a882597c9
MatthewGerber <gerber.matthew@gmail.com>
2018-02-12 01:21:26 -0500

    Updated.

--- src/sys2202/io/FileSystem.java
+++ src/sys2202/io/FileSystem.java
@@ -9,18 +9,20 @@ public class FileSystem {
 
 	public static void main(String[] args) throws IOException {
 
-		// get all file paths under a particular directory
+		// get all file paths at or below a particular directory that have a .json file extension
 		ArrayList<String> paths = new ArrayList<String>();
-		Files.walk(Paths.get("/Users/matthewgerber/Desktop/sp2017sys2202_mobile_data")).filter(Files::isRegularFile)
-		                                                                               .map(path -> path.toString())
-		                                                                               .filter(path -> path.endsWith(".json"))
-				                                                                       .forEach(paths::add);
+		Files.walk(Paths.get("/Users/matthewgerber/Desktop/sys2202_mobile_data")).filter(Files::isRegularFile)
+		                                                                         .map(path -> path.toString())
+		                                                                         .filter(path -> path.endsWith(".json"))
+				                                                                 .forEach(paths::add);
 		
-		// print the paths
+		// do something with the paths. the example below simply prints each path to the console.
 		for(String path : paths) {
+			
 			System.out.println("\t" + path);
+			
 		}
 		
-		System.out.println("There were " + paths.size() + " path(s):");
+		System.out.println("There were " + paths.size() + " path(s).");
 	}
 }
